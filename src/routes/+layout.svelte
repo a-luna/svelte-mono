89e41e8f68306e3574f03d1eb50a6acba87ea18c
Aaron Luna <contact@aaronluna.dev>
2022-08-31 00:40:46 -0700

    fix: :bug: fix: sveltekit config setting breaking changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../global.css';
+
+	export const ssr = false;
 </script>
 
 <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '200.html',
+		}),
+		prerender: { entries: [] },
 	},
 };
 
