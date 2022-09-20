6be4c40ecd8286021e642356e04f31b4d58906c4
Aaron Luna <contact@aaronluna.dev>
2022-09-20 09:08:45 -0700

    fix: :bug: ensure header names are consistent throughout project

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,8 @@
-import type { PageLoad } from "./$types";
+import type { PageLoad } from './$types';
 
 export async function load({ data, setHeaders }): Promise<PageLoad> {
-  setHeaders({
-		'cache-control': 'max-age=3600'
+	setHeaders({
+		'Cache-Control': 'max-age=3600'
 	});
-  return { allRepos: data.allRepos };
+	return { allRepos: data.allRepos };
 }
--- src/routes/projects/[repo]/+page.ts
+++ src/routes/projects/[repo]/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export async function load({ data, setHeaders }): Promise<PageLoad> {
 	setHeaders({
-		'cache-control': 'max-age=3600'
+		'Cache-Control': 'max-age=3600'
 	});
 	return { readme: data.readme };
 }
