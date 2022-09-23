45ba3a12bacea8e929c03997d9f45aab2a5ba76c
Aaron Luna <contact@aaronluna.dev>
2022-09-22 22:15:23 -0700

    ci: :green_heart: add --legacy-peer-deps flag to install command

--- netlify-plugin-pnpm/index.js
+++ netlify-plugin-pnpm/index.js
@@ -1,7 +1,7 @@
 export const onPreBuild = async ({ utils: { build, run } }) => {
 	try {
 		await run.command('npm install -g pnpm');
-		await run.command('pnpm install --frozen-lockfile=false');
+		await run.command('pnpm install --frozen-lockfile=false --legacy-peer-deps');
 	} catch (error) {
 		return build.failBuild(error);
 	}
