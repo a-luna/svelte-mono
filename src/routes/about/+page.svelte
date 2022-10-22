5c4ae555485845e989cc155fe99dcd6a4ab67639
Aaron Luna <contact@aaronluna.dev>
2022-10-22 11:14:41 -0700

    feat: :sparkles: add photo to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,7 @@
 	import Code from '$lib/components/Icons/Code.svelte';
 	import Handshake from '$lib/components/Icons/Handshake.svelte';
 	import Keyboard from '$lib/components/Icons/Keyboard.svelte';
-	import { SITE_TITLE } from '$lib/siteConfig';
+	import { SITE_TITLE, SITE_URL } from '$lib/siteConfig';
 </script>
 
 <svelte:head>
@@ -10,15 +10,16 @@
 	<meta name="description" content={`About Me`} />
 </svelte:head>
 
+<div class="my-photo"><img src="{SITE_URL}/AaronLuna.jpg" alt="" /></div>
 <div class="about-me-bullets prose prose-invert">
-	<div class="bullet"><Keyboard /></div>
-	<span
+	<div class="bullet bullet-1"><Keyboard /></div>
+	<span class="about-me-1"
 		>I am a software engineer living and working in Reno, NV. I enjoy music and sports (especially
 		baseball and basketball) and I am especially interested in the changing landscape of legalized
 		gambling on sporting events.</span
 	>
-	<div class="bullet"><Code /></div>
-	<span
+	<div class="bullet bullet-2"><Code /></div>
+	<span class="about-me-2"
 		>I have 10+ years of experience developing software, starting out as a <strong>C#</strong>
 		programmer before moving to web development in 2016. Whenever possible, I prefer to work with vanilla
 		<strong>JavaScript (ES5/6)</strong>,
@@ -41,8 +42,8 @@
 			<strong>SQLAlchemy</strong>.</span
 		></span
 	>
-	<div class="bullet"><Handshake /></div>
-	<span
+	<div class="bullet bullet-3"><Handshake /></div>
+	<span class="about-me-3"
 		>I am always interested in hearing about new opportunities and taking on new projects, feel free
 		to contact me
 		<a href="mailto:contact@aaronluna.dev" target="_blank">via email</a>,
@@ -62,9 +63,40 @@
 		column-gap: 1rem;
 		margin: 0 auto;
 		line-height: 1.6;
+		max-width: 700px;
+		padding: 0 1rem;
+	}
+	.my-photo {
+		margin: 1rem auto 2rem auto;
+		border-radius: 50%;
+		width: 150px;
 	}
 	.bullet {
 		width: 25px;
 		margin: 4px 0 0 0;
+	}
+	.bullet-1 {
+		grid-column: 1 / span 1;
+		grid-row: 1 / span 1;
+	}
+	.about-me-1 {
+		grid-column: 2 / span 1;
+		grid-row: 1 / span 1;
+	}
+	.bullet-2 {
+		grid-column: 1 / span 1;
+		grid-row: 2 / span 1;
+	}
+	.about-me-2 {
+		grid-column: 2 / span 1;
+		grid-row: 2 / span 1;
+	}
+	.bullet-3 {
+		grid-column: 1 / span 1;
+		grid-row: 3 / span 1;
+	}
+	.about-me-3 {
+		grid-column: 2 / span 1;
+		grid-row: 3 / span 1;
 	}
 </style>
