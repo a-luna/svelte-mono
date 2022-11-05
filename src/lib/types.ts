ab4480f7ac50197763edbfd0e0265d266c5404db
Aaron Luna <contact@aaronluna.dev>
2022-11-05 06:38:59 -0700

    fix: :bug: add cached user repo data, used when offline since github api will not be available

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -123,10 +123,7 @@ export interface TutorialSection extends BlogPost {
 	url_git_rel_diff: string;
 }
 
-export type TutorialSectionNumberMap = Pick<
-	TutorialSection,
-	'slug' | 'lead' | 'series_part' | 'series_weight'
->;
+export type TutorialSectionNumberMap = Pick<TutorialSection, 'slug' | 'lead' | 'series_part' | 'series_weight'>;
 
 export interface GHUser {
 	login: string;
@@ -150,96 +147,88 @@ export interface GHUser {
 }
 
 export interface GHRepo {
-	id: number;
-	node_id: string;
+	id?: number;
+	node_id?: string;
 	name: string;
-	full_name: string;
-	private: boolean;
-	owner: GHUser;
+	full_name?: string;
+	private?: boolean;
+	owner?: GHUser;
 	html_url: string;
 	description: string;
-	fork: boolean;
-	url: string;
+	fork?: boolean;
+	url?: string;
 	forks_url: string;
-	keys_url: string;
-	collaborators_url: string;
-	teams_url: string;
-	hooks_url: string;
-	issue_events_url: string;
-	events_url: string;
-	assignees_url: string;
-	branches_url: string;
-	tags_url: string;
-	blobs_url: string;
-	git_tags_url: string;
-	git_refs_url: string;
-	trees_url: string;
-	statuses_url: string;
-	languages_url: string;
+	keys_url?: string;
+	collaborators_url?: string;
+	teams_url?: string;
+	hooks_url?: string;
+	issue_events_url?: string;
+	events_url?: string;
+	assignees_url?: string;
+	branches_url?: string;
+	tags_url?: string;
+	blobs_url?: string;
+	git_tags_url?: string;
+	git_refs_url?: string;
+	trees_url?: string;
+	statuses_url?: string;
+	languages_url?: string;
 	stargazers_url: string;
-	contributors_url: string;
-	subscription_url: string;
-	subscribers_url: string;
-	git_commits_url: string;
-	commits_url: string;
-	issue_comment_url: string;
-	comments_url: string;
-	compare_url: string;
-	contents_url: string;
-	archive_url: string;
-	merges_url: string;
-	issues_url: string;
-	downloads_url: string;
-	milestones_url: string;
-	pulls_url: string;
-	labels_url: string;
-	notifications_url: string;
-	deployments_url: string;
-	releases_url: string;
-	created_at: string;
-	updated_at: string;
-	pushed_at: string;
-	git_url: string;
-	ssh_url: string;
-	clone_url: string;
-	svn_url: string;
-	homepage: string;
-	size: number;
+	contributors_url?: string;
+	subscription_url?: string;
+	subscribers_url?: string;
+	git_commits_url?: string;
+	commits_url?: string;
+	issue_comment_url?: string;
+	comments_url?: string;
+	compare_url?: string;
+	contents_url?: string;
+	archive_url?: string;
+	merges_url?: string;
+	issues_url?: string;
+	downloads_url?: string;
+	milestones_url?: string;
+	pulls_url?: string;
+	labels_url?: string;
+	notifications_url?: string;
+	deployments_url?: string;
+	releases_url?: string;
+	created_at?: string;
+	updated_at?: string;
+	pushed_at?: string;
+	git_url?: string;
+	ssh_url?: string;
+	clone_url?: string;
+	svn_url?: string;
+	homepage?: string;
+	size?: number;
 	stargazers_count: number;
-	watchers_count: number;
-	language: string;
-	has_issues: boolean;
-	has_projects: boolean;
-	has_downloads: boolean;
-	has_wiki: boolean;
-	has_pages: boolean;
+	watchers_count?: number;
+	language?: string;
+	has_issues?: boolean;
+	has_projects?: boolean;
+	has_downloads?: boolean;
+	has_wiki?: boolean;
+	has_pages?: boolean;
 	forks_count: number;
-	mirror_url: string;
-	archived: boolean;
-	disabled: boolean;
-	open_issues_count: number;
-	license: string;
-	allow_forking: boolean;
-	is_template: boolean;
-	web_commit_signoff_required: boolean;
-	topics: string[];
-	visibility: 'public' | 'private';
-	forks: number;
-	open_issues: number;
-	watchers: number;
-	default_branch: string;
-	temp_clone_token: string;
-	network_count: number;
-	subscribers_count: number;
+	mirror_url?: string;
+	archived?: boolean;
+	disabled?: boolean;
+	open_issues_count?: number;
+	license?: string;
+	allow_forking?: boolean;
+	is_template?: boolean;
+	web_commit_signoff_required?: boolean;
+	topics?: string[];
+	visibility?: 'public' | 'private';
+	forks?: number;
+	open_issues?: number;
+	watchers?: number;
+	default_branch?: string;
+	temp_clone_token?: string;
+	network_count?: number;
+	subscribers_count?: number;
 }
-
-export type JsonValue =
-	| string
-	| number
-	| boolean
-	| null
-	| JsonValue[]
-	| { [key: string]: JsonValue };
 
 export type HttpMethod = typeof HTTP_METHODS[number];
 export type IconName = typeof ICON_NAMES[number];
