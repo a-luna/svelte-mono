55f8df232436d35e3d134d0b576e1605d880a4bf
Aaron Luna <contact@aaronluna.dev>
2022-12-28 10:58:24 -0800

    fix: :bug: make /projects route load function run on server only

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,9 +1,9 @@
 import { cacheIsStale, convertGHRepos } from '$lib/projectMetaData';
 import { userRepos } from '$lib/stores';
 import { get } from 'svelte/store';
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
 	const { repos, cachedAt } = get(userRepos);
 	if (!repos.length || cacheIsStale(cachedAt)) {
 		const projectData = await fetch('/repos.json').then((r) => r.json());
