bf26f6ee108d1509f32de9197178215f7a15d250
Aaron Luna <contact@aaronluna.dev>
2023-02-02 19:27:01 -0800

    fix: :chart_with_upwards_trend: analytics script should not be on page in dev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import '../global.css';
-
-	export const ssr = false;
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<script
+			async
+			defer
+			data-website-id="cb918dba-fc25-40be-ac0e-b54818fce446"
+			src="https://aluna-umami.netlify.app/umami.js"
+		></script>
+	{/if}
+</svelte:head>
 
 <slot />
