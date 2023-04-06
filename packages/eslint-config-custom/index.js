cda9260793ff3f509cd8ed8fffc01e469356a2e7
Aaron Luna <contact@aaronluna.dev>
2023-04-05 21:28:38 -0700

    chore(eslint-config): :sparkles: add eslint-config-custom package to monorepo

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+	extends: ['eslint:recommended',, 'prettier'],
+	plugins: ['svelte3'],
+  rules: {
+    // tweak custom rules here
+    // "@next/next/no-html-link-for-pages": "off",
+    // "react/jsx-key": "off",
+  },
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
+};
