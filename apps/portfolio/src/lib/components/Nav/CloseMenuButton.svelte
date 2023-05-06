472fc90222a62327acd21eb7bab75e422bfd8b38
Aaron Luna <contact@aaronluna.dev>
2023-05-06 05:35:18 -0700

    feat(portfolio): :zap: create CloseMenuButton component

---
+++ apps/portfolio/src/lib/components/Nav/CloseMenuButton.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { mobileNavOpen } from '$lib/stores';
+	import { BasicIconRenderer } from '@a-luna/shared-ui';
+</script>
+
+<button
+	class="menu-button"
+	class:open
+	aria-label="menu"
+	tabindex="0"
+	aria-controls="nav-sidebar"
+	aria-haspopup="menu"
+	aria-expanded={$mobileNavOpen}
+	on:click={() => ($mobileNavOpen = !$mobileNavOpen)}
+>
+	<BasicIconRenderer icon={'close'} width={'26px'} height={'26px'} />
+</button>
+
+<style lang="postcss">
+	.menu-button {
+		display: none;
+		padding: 0.75rem;
+		border-width: 3px;
+		border-style: solid;
+		border-color: var(--white);
+		background-color: var(--black-tint2);
+		color: var(--white);
+		font-size: 2rem;
+		z-index: 5;
+	}
+	.menu-button:hover {
+		border-color: var(--accent-color);
+		color: var(--accent-color);
+	}
+	.menu-button.open {
+		display: block;
+	}
+
+	@media (min-width: 768px) {
+		.menu-button {
+			display: none;
+		}
+	}
+</style>
