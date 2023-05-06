3c98f3e35867f9f7132ad3a227e7e9f2e7efd461
Aaron Luna <contact@aaronluna.dev>
2023-05-06 05:34:15 -0700

    feat(portfolio): :zap: create ApiTutorial component

---
+++ apps/portfolio/src/lib/components/HomePage/ApiTutorial.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<div>
+	<h2 class="underline--magical">Flask API Tutorial</h2>
+	<p class="normal-text">
+		<span><a href="series/flask-api-tutorial/overview">Flask API Tutorial</a></span>
+	</p>
+</div>
+
+<style lang="postcss">
+	.normal-text {
+		font-size: 0.9rem;
+		margin: 1.5rem 0 0 0;
+	}
+	.normal-text span {
+		color: var(--white-shade2);
+		background-color: hsl(0 0% 0% / 0.2);
+		font-weight: 400;
+	}
+</style>
