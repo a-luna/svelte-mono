27aaee70b665561834a44f3a0a1d39b01feac9cc
Aaron Luna <contact@aaronluna.dev>
2023-07-07 05:53:45 -0700

    refactor(shared-ui): :wrench: remove dead/commented code from vite.config.ts

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,6 +1,5 @@
-/// <reference types="vitest" />
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -8,10 +7,6 @@ export default defineConfig({
 		globals: false,
 		environment: 'jsdom',
 		dir: './src',
-		// moduleNameMapper: {
-		// 	'^\\$lib(.*)$': '<rootDir>/src/lib$1',
-		// 	'^\\$app(.*)$': ['<rootDir>/.svelte-kit/dev/runtime/app$1', '<rootDir>/.svelte-kit/build/runtime/app$1'],
-		// },
 		coverage: {
 			skipFull: true,
 		},
