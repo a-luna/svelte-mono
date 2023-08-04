e51347258ab999a450dd0103377bab4519bb0eb5
Aaron Luna <contact@aaronluna.dev>
2023-08-03 23:18:01 -0700

    refactor(portfolio): :recycle: remove code that was moved to ExpandableSection, ensure toggleSection event is bubbled up

--- apps/portfolio/src/lib/components/TableOfContents/TableOfContents.svelte
+++ apps/portfolio/src/lib/components/TableOfContents/TableOfContents.svelte
@@ -1,35 +1,15 @@
 <script lang="ts">
-	import { beforeNavigate } from '$app/navigation';
 	import TocSection from '$lib/components/TableOfContents/TocSection.svelte';
 	import type { TocSection as TocSectionType } from '$lib/types';
 	import { getRandomHexString } from '$lib/util';
-	import { createEventDispatcher } from 'svelte';
 	import ExpandableSection from '../ExpandableSection.svelte';
 
 	export let toc: TocSectionType[];
-	export let detailsElement: HTMLDetailsElement | undefined = undefined;
 	export const id = `toc-${getRandomHexString(4)}`;
-	let open = false;
-
-	const toggleDetailsElement = createEventDispatcher<{
-		toggleSection: { sectionId: string };
-	}>();
-
-	function handleSectionToggled() {
-		if (!detailsElement) return;
-		open = detailsElement.open;
-		toggleDetailsElement('toggleSection', { sectionId: id });
-	}
-
-	beforeNavigate(() => {
-		if (detailsElement) {
-			detailsElement.open = false;
-		}
-	});
 </script>
 
 {#if toc.length}
-	<ExpandableSection sectionId={'table-of-contents'} title={'Table of Contents'}>
+	<ExpandableSection sectionId={'table-of-contents'} title={'Table of Contents'} on:toggleSection>
 		<ul>
 			{#each toc as section}
 				<TocSection {section} />
