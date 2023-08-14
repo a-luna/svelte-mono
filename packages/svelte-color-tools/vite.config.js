9e5dc104d8b89f8e769e5a484d4bd06a73db75af
Aaron Luna <contact@aaronluna.dev>
2023-08-13 17:57:39 -0700

    chore(svelte-color-tools): :wrench: fix import issue in vite.config.js

--- packages/svelte-color-tools/vite.config.js
+++ packages/svelte-color-tools/vite.config.js
@@ -1,6 +1,8 @@
 /// <reference types="vitest" />
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -10,13 +12,18 @@ export default defineConfig({
 	test: {
 		globals: false,
 		environment: 'jsdom',
-		dir: './src',
 		moduleNameMapper: {
 			'^\\$lib(.*)$': '<rootDir>/src/lib$1',
 			'^\\$app(.*)$': ['<rootDir>/.svelte-kit/dev/runtime/app$1', '<rootDir>/.svelte-kit/build/runtime/app$1'],
 		},
 		coverage: {
 			skipFull: true,
+			exclude: [...configDefaults.exclude, '**/b64Encode.test/**', '**/tests/**', '**/.svelte-kit/**'],
+		},
+	},
+	resolve: {
+		alias: {
+			'@a-luna/shared-ui': resolve(__dirname, 'node_modules/@a-luna/shared-ui'),
 		},
 	},
 });
