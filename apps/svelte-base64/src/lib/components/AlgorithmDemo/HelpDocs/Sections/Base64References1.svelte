a72366179f5aed90d4bd2645e30cad7122ffb12d
Aaron Luna <contact@aaronluna.dev>
2024-01-28 14:27:16 -0800

    refactor(svelte-base64): :recycle: refactor help section references in AlgorithmDemo

--- apps/svelte-base64/src/lib/components/AlgorithmDemo/HelpDocs/Sections/Base64References1.svelte
+++ apps/svelte-base64/src/lib/components/AlgorithmDemo/HelpDocs/Sections/Base64References1.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import { references } from '$lib/components/AlgorithmDemo/HelpDocs/Sections/_helpText';
-	const thisRef = references[0];
-	const { title, url, description } = thisRef;
+	import { HelpReference1 } from '$lib/components/AlgorithmDemo/HelpDocs/Sections/_helpText';
+	const { title, url, description } = HelpReference1;
 </script>
 
 <div class="help-module">
@@ -10,7 +9,7 @@
 			<div class="b64-ref-heading">
 				<h4>{title}</h4>
 			</div>
-			<div class="b64-ref-url"><span class="b64-ref-label">src:</span><a href={url}>{url}</a></div>
+			<div class="b64-ref-url"><span class="b64-ref-label">source:</span><a href={url}>{url}</a></div>
 			<div class="b64-ref-content">
 				{#each description as text}
 					<p>{@html text}</p>
---
+++ apps/svelte-base64/src/lib/components/AlgorithmDemo/HelpDocs/Sections/index.ts
@@ -0,0 +1,37 @@
+import Base64OutputEncoding from './Base64OutputEncoding.svelte';
+import Base64References1 from './Base64References1.svelte';
+import Base64References2 from './Base64References2.svelte';
+import Base64StandardAlphabet from './Base64StandardAlphabet.svelte';
+import Base64UrlAlphabet from './Base64UrlAlphabet.svelte';
+import KeyboardShortcuts from './KeyboardShortcuts.svelte';
+import NavButtons from './NavButtons.svelte';
+import StringInputEncoding1 from './StringInputEncoding1.svelte';
+import StringInputEncoding2 from './StringInputEncoding2.svelte';
+import StringInputEncodingAscii from './StringInputEncodingAscii.svelte';
+import StringInputEncodingBinary from './StringInputEncodingBinary.svelte';
+import StringInputEncodingHex from './StringInputEncodingHex.svelte';
+import StringInputEncodingUtf8 from './StringInputEncodingUTF8.svelte';
+import WhatIsBase64 from './WhatIsBase64.svelte';
+import WhatIsntBase64 from './WhatIsntBase64.svelte';
+import WhyBase641 from './WhyBase641.svelte';
+import WhyBase642 from './WhyBase642.svelte';
+
+export {
+	Base64OutputEncoding,
+	Base64References1,
+	Base64References2,
+	Base64StandardAlphabet,
+	Base64UrlAlphabet,
+	KeyboardShortcuts,
+	NavButtons,
+	StringInputEncoding1,
+	StringInputEncoding2,
+	StringInputEncodingAscii,
+	StringInputEncodingBinary,
+	StringInputEncodingHex,
+	StringInputEncodingUtf8,
+	WhatIsBase64,
+	WhatIsntBase64,
+	WhyBase641,
+	WhyBase642,
+};
